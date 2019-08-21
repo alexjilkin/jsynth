339f9d971af63a5c9d5728e04367b55a3f35d54b
alexj <jilkinalex@gmail.com>
2019-08-21 22:18:47 +0300

    Fix styles

--- src/modules/oscillator/Oscillator.jsx
+++ src/modules/oscillator/Oscillator.jsx
@@ -1,14 +1,14 @@
 import React, {useState} from 'react';
 import "./logic"
 
+import styles from './Oscillator.css'
 
-import './Oscillator.css'
-
+console.log(styles)
 const Oscillator = (props) => {
     const [isOn, setOn] = useState(false);
 
     return(
-        <div styleName="container">
+        <div className={styles.container}>
             <div>An oscialltor.</div>
             <div styleName={isOn ? "on" : "off"}></div>
         </div>
