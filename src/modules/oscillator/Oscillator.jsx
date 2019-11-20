83e21d9eafe1f596332d06fab58d7d27924236f1
Alex jilkin <jilkinalex@gmail.com>
2019-11-21 00:25:07 +0200

    Add another light

--- src/modules/oscillator/Oscillator.jsx
+++ src/modules/oscillator/Oscillator.jsx
@@ -20,35 +20,49 @@ const defaultState = {
     is5thOn: false
 }
 
-const orbitRadius = 10;
+const orbitRadius = 12;
 
-function Cube({onXChange, onYChange, onZChange}) {
-    const ref = useRef()
-    useEffect(() => {
-        const scene = new THREE.Scene();
-        scene.background = new THREE.Color( 0x6ed3cf );
-        const camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 10);
+const addLights = (scene) => {
+    let light = new THREE.DirectionalLight( 0xffffff );
+    light.position.set( 50, 20, 40 )
+    scene.add( light );
 
-        const renderer = new THREE.WebGLRenderer({ antialias: true });
-        renderer.setSize( 150,150 );
-        const geometry = new THREE.BoxGeometry( 5, 5, 5 );
+    light = new THREE.DirectionalLight( 0xffffff );
+    light.position.set( -10, 0, -10 )
+    scene.add( light );
+}
+
+const setControls = (camera, element) => {
+    const controls = new OrbitControls( camera, element );
+    controls.enableZoom = false;
+    controls.enablePan = false;
+    controls.rotateSpeed = 0.1;
+    controls.update();
+}
+const addCube = (scene) => {
+    const geometry = new THREE.BoxGeometry( 7, 7, 7 );
         const cube = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( { color: 0xff0000 } ) )
         cube.rotateX(1)
         cube.rotateX(1)
         scene.add(cube);
+}
+function Cube({onXChange, onYChange, onZChange}) {
+    const ref = useRef()
 
-        var light = new THREE.DirectionalLight( 0xffffff );
-		light.position.set( 50, 20, 40 )
-        scene.add( light );
+    useEffect(() => {
+        const scene = new THREE.Scene();
+        scene.background = new THREE.Color( 0x6ed3cf );
+        const camera = new THREE.PerspectiveCamera( 75, 1, 0.1, 20);
+        const renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setSize( 150,150 );
+
+        addLights(scene);
+        addCube(scene)
 
         camera.position.z = orbitRadius;
         ref.current.appendChild( renderer.domElement );
 
-        const controls = new OrbitControls( camera, renderer.domElement );
-        controls.enableZoom = false;
-        controls.enablePan = false;
-        controls.rotateSpeed = 0.1;
-        controls.update();
+        setControls(camera, renderer.domElement);
         
         let lastX;
         let lastY;
