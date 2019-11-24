e57ae001b0da2a0fe5d332402fb6db5f1e2c6652
alexj <jilkinalex@gmail.com>
2019-11-24 20:48:07 +0200

    Update demos state

--- src/synth/consts.js
+++ src/synth/consts.js
@@ -3,4 +3,7 @@ export const ItemTypes = {
 }
 export const sampleRate = 44100;
 
-export const demoState = [[{"module":"Oscillator","persistentState":{"frequency":440,"isSquareOn":false,"isSineOn":true,"isSawOn":true,"isFirstOn":false,"is3rdOn":false,"is5thOn":true}},{"module":"Sequencer","persistentState":{"sequence":[true,true,false,null,null,null,true,null],"bpm":120,"sequenceSize":8,"isEnvelopeOn":true}},{"module":"Delay"}],[{"module":"Oscilloscope"}]]
+export const demoState = [
+    [{"module":"Oscillator","persistentState":{"frequency":440,"isSquareOn":false,"isSineOn":true,"isSawOn":true,"isFirstOn":false,"is3rdOn":false,"is5thOn":true}},{"module":"Sequencer","persistentState":{"sequence":[true,true,false,null,null,null,true,null],"bpm":120,"sequenceSize":8,"isEnvelopeOn":true}},{"module":"Delay"}]
+    ,[{"module":"Oscillator","persistentState":{"frequency":440,"isSquareOn":false,"isSineOn":true,"isSawOn":true,"isFirstOn":true,"is3rdOn":false,"is5thOn":false}},{"module":"Sequencer","persistentState":{"sequence":[false,false,false,true,null,true,true,null],"bpm":120,"sequenceSize":8,"isEnvelopeOn":true}}]
+,[{"module":"Oscilloscope"}]]
