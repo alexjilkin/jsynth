aad69b40965c3c1f2d1fd11ff28ab9d5501fd204
Alex jilkin <jilkinalex@gmail.com>
2020-01-24 21:34:51 +0200

    new demo state

--- src/synth/consts.js
+++ src/synth/consts.js
@@ -3,7 +3,7 @@ export const ItemTypes = {
 }
 export const sampleRate = 44100;
 
-export const demoState = [[{"module":"Sequencer","persistentState":{"sequence":[2,2,1,3,null,null,null,null],"bpm":120,"sequenceSize":8,"isEnvelopeOn":true}},{"module":"Oscillator","persistentState":{"frequency":420,"sawAmount":0.5000000000000001,"squareAmount":6.845983728302534e-17,"sineAmount":1}},{"module":"Delay"}],[],[{"module":"Oscilloscope"},{"module":"FrequencyView"}]]
+export const demoState = [[{"module":"Sequencer","persistentState":{"sequence":[2,2,1,3,null,null,null,null],"bpm":120,"sequenceSize":8,"isEnvelopeOn":true}},{"module":"Oscillator","persistentState":{"frequency":440,"sawAmount":0.5000000000000001,"squareAmount":6.845983728302534e-17,"sineAmount":1}},{"module":"Delay","persistentState":{"isOn":true,"delayAmount":0.25,"delayDepth":5,"gain":0.5}}],[{"module":"Sequencer","persistentState":{"sequence":[0,0,3,null,null,null,null,5],"bpm":120,"sequenceSize":8,"isEnvelopeOn":true}},{"module":"Oscillator","persistentState":{"frequency":440,"sawAmount":0.5000000000000001,"squareAmount":6.845983728302534e-17,"sineAmount":1}}],[{"module":"Oscilloscope"},{"module":"FrequencyView"}]]
 
 export const basicGroup = [{module: 'Oscillator', func: bypassFunction}, {module: 'Sequencer', func: bypassFunction}];
 export const basicMasterGroup = [{module: 'Oscilloscope', func: bypassFunction}, {module: 'FrequencyView', func: bypassFunction}];
