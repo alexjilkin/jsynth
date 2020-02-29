b6b351454e51dfdfc75da853bbfda85f695184fc
Alex jilkin <jilkinalex@gmail.com>
2020-02-29 20:34:06 +0200

    Warm up audio

--- src/output/BrowserPlayer.js
+++ src/output/BrowserPlayer.js
@@ -28,6 +28,42 @@ export const play = (waveGenerator) => {
   return () => isPlaying = false
 }
 
+
+
+const warmUpAudio = () => {
+	window.AudioContext = window.AudioContext || window.webkitAudioContext;
+	if (window.AudioContext) {
+		window.audioContext = new window.AudioContext();
+	}
+	var fixAudioContext = function (e) {
+		if (window.audioContext) {
+			// Create empty buffer
+			var buffer = window.audioContext.createBuffer(1, 1, 22050);
+			var source = window.audioContext.createBufferSource();
+			source.buffer = buffer;
+			// Connect to output (speakers)
+			source.connect(window.audioContext.destination);
+			// Play sound
+			if (source.start) {
+				source.start(0);
+			} else if (source.play) {
+				source.play(0);
+			} else if (source.noteOn) {
+				source.noteOn(0);
+			}
+		}
+		// Remove events
+		document.removeEventListener('touchstart', fixAudioContext);
+		document.removeEventListener('touchend', fixAudioContext);
+	};
+	// iOS 6-8
+	document.addEventListener('touchstart', fixAudioContext);
+	// iOS 9
+	document.addEventListener('touchend', fixAudioContext);
+}
+
+warmUpAudio();
+
 export default {
   play
 }
