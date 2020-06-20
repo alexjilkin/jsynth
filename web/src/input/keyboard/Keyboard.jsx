edadd9c9c82067dfb5851ee90605390c78736128
Alex <alex.@MacBook-Pro.local>
2020-06-20 16:32:12 +0300

    Add press ui on the keyboard

--- web/src/input/keyboard/Keyboard.jsx
+++ web/src/input/keyboard/Keyboard.jsx
@@ -1,35 +1,35 @@
-import React, {useEffect, useCallback, useState, useMemo} from 'react' 
+import React, {useCallback, useState, useMemo} from 'react' 
 import {
     isMobile
   } from "react-device-detect";
 import './Keyboard.scss';
-import {initKeyboardInput, virtualKeyboardKeyDown, virtualKeyboardKeyUp} from './utils'
+import {useKeyboardInput, virtualKeyboardKeyDown, virtualKeyboardKeyUp} from './utils'
 
 // 1 for white, 0 for black
 const octaveLayout = [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1]
 const indexToKey = ['A', 'W', 'S', 'E', 'D', 'F', 'T', 'G', 'Y', 'H', 'U', 'J', 'K', 'O', 'L']
+const keyCodeToIndex = [65, 87, 83, 69, 68, 70, 84, 71, 89, 72, 85, 74, 75, 79, 76]
 const whiteKeyWidth = 40;
 const blackKeyWidth = 25;
 
 const Keyboard = ({numberOfOctaves = 2}) => {
-    useEffect(() => {
-        initKeyboardInput()
-    }, [])
-    
     const keyboardLayout = useMemo(() => getKeyboardLayout(numberOfOctaves), [numberOfOctaves])
+    const [playingKeysByKeycode] = useKeyboardInput()
+
+    const playingKeyCodes = useMemo(() => Object.keys(playingKeysByKeycode).filter(keyCode => playingKeysByKeycode[keyCode]).map(keyCode => parseInt(keyCode)), [playingKeysByKeycode])
 
     return (
         <div styleName="container" style={{gridAutoColumns: whiteKeyWidth}}>
             {keyboardLayout.map((value, index) =>
                 value ? 
-                    <WhiteKey key={index} index={index}/> :
-                    <BlackKey key={index} index={index} keyboardLayout={keyboardLayout} />
+                    <WhiteKey key={index} isPressed={playingKeyCodes.some(playingKeyCode => keyCodeToIndex.indexOf(playingKeyCode) === index)} index={index}/> :
+                    <BlackKey key={index} isPressed={playingKeyCodes.some(playingKeyCode => keyCodeToIndex.indexOf(playingKeyCode) === index)} index={index} keyboardLayout={keyboardLayout} />
             )}
         </div>
     )
 }
 
-const BlackKey = ({index, keyboardLayout}) => {
+const BlackKey = ({index, isPressed, keyboardLayout}) => {
     const eventHandlers = useMemo(() => isMobile ? getTouchEvents(index) : getMouseEvents(index), [index])
     const numberOfWhiteKeysBefore = useMemo(() => getNumberOfWhiteKeysBefore(keyboardLayout, index), [index, keyboardLayout])
 
@@ -37,7 +37,7 @@ const BlackKey = ({index, keyboardLayout}) => {
         <div 
             {...(eventHandlers)}
             styleName="black-key" 
-            style={{left: `${(numberOfWhiteKeysBefore * whiteKeyWidth) - blackKeyWidth / 2}px`, width: blackKeyWidth}}
+            style={{left: `${(numberOfWhiteKeysBefore * whiteKeyWidth) - blackKeyWidth / 2}px`, width: blackKeyWidth, backgroundColor: isPressed ? 'grey' : 'black'}}
         >
             <span styleName="key-notation">{indexToKey[index] || ''}</span>
             <div styleName="white-line"></div>
@@ -45,12 +45,10 @@ const BlackKey = ({index, keyboardLayout}) => {
     )
 }
 
-const WhiteKey = ({index}) => {
-    const [isPressed, setIsPressed] = useState(false)
-
+const WhiteKey = ({index, isPressed}) => {
     const getEventHandlers = useCallback(() => isMobile ? getTouchEvents(index) : getMouseEvents(index), [index])
     return (
-        <div {...(getEventHandlers())} styleName="white-key">
+        <div {...(getEventHandlers())} styleName="white-key" style={{backgroundColor: isPressed ? 'lightgrey' : 'white'}}>
             <span styleName="key-notation">{indexToKey[index] || ''}</span>
         </div>
     )
